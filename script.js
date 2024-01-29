d6b9e11d2799b5c0a6dc3cee27ee8ee1b474b2e4
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-01-29 16:36:57 -0500

    prime number

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
- var n="10";
-
- for(i=0;i<=10;i++)
- {
-    if(i%2==0){
-        console.log(i)}
- }
+// to find the prime number
+var n=8;
+var flag=0;
+for(i=2;i<n/2;i++){
+    if(n%2==0){
+        flag=1;
+        break;}
+    }
+if(flag==0){
+        console.log("its prime ")
+    }
+else{
+    console.log("its not a prime")
+}
